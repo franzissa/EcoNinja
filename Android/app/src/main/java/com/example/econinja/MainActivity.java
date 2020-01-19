e170de231e251abe6a5683c91f2c962b1625b061
glanonim <6484926+glanonim@users.noreply.github.com>
2020-01-19 23:06:10 +0100

    Added settings

--- Android/app/src/main/java/com/example/econinja/MainActivity.java
+++ Android/app/src/main/java/com/example/econinja/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.econinja;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide(); // hide the title bar
+    }
+
+    public void showRegister(View view) {
+        //setContentView(R.layout.activity_challenge);
     }
 }
