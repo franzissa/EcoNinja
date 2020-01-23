8b6706ab37d1a115d9fc3c39810c680fa49e58e1
Franziska Schmidt <franzisssa@gmail.com>
2020-01-23 19:58:45 +0100

    Second Version with bad naming
    quick fix

--- Android 2/MyApplication4/app/src/main/java/com/example/myapplication4/MainActivity.java
+++ Android 2/MyApplication4/app/src/main/java/com/example/myapplication4/MainActivity.java
@@ -1,4 +1,8 @@
-package com.example.econinja;
+package com.example.myapplication4;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -6,30 +10,21 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.os.Bundle;
-import android.view.View;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide(); // hide the title bar#
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.setting, R.id.shared)
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
-
-    public void showRegister(View view) {
-        setContentView(R.layout.activity_main_menu);
-    }
 }
